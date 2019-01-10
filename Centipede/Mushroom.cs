be28d0b92741778c43608e1abe2978cbd6ca6a6d
Bl147206 <brady.lewis.206@k12.friscoisd.org>
2019-01-10 11:14:19 -0600

    Initial commit

---
+++ Centipede/Mushroom.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+namespace Centipede
+{
+    class Mushroom
+    {
+        Texture2D tex;
+        Rectangle loc;
+        int damage;
+        bool isDestroyed;
+        
+        public Mushroom(int x,int y)
+        {
+            tex = null;
+            loc = new Rectangle(x,y,10,10);
+            damage = 0;
+            isDestroyed = false;
+        }
+
+        public void setTex(Texture2D t)
+        {
+            tex = t;
+        }
+
+        public void hit()
+        {
+            damage++;
+            if(damage>2)
+            {
+                isDestroyed = true;
+            }
+        }
+
+
+    }
+}
